ff07bf1ec691cb9ef5ef56409107b1abdee56889
d.yash <happpy8036@gmail.com>
2026-09-19 16:05:03 +0530

    Prepare Flask app for deployment

--- app.py
+++ app.py
@@ -91,6 +91,11 @@ def create_app():
     return app
 
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(
+        debug=False,
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
